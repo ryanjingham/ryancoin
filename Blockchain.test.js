6d540af2cf170c1b0a725ce769e0eb8b249b7fc4
Ryan Ingham <ryaningham@Ryans-MacBook-Air.local>
2018-12-25 00:05:41 +0700

    push from macbook air

--- Blockchain.test.js
+++ Blockchain.test.js
@@ -4,10 +4,11 @@ const Blockchain = require('./Blockchain');
 const Block = require('./Block');
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('start with genesis block', () => {
@@ -20,5 +21,16 @@ describe('Blockchain', () => {
 
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
     });
+    it('validates a valid chain',() => {
+        bc2.addBlock('yoted');
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt block'), () => {
+        bc2.chain[0].data = 'bad data';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    };
 
 });
