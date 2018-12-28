51cb3602e855a4511203f35ae0e98dd3e39edf11
Ryan Ingham <ryaningham2001@gmail.com>
2018-12-28 15:25:28 +0700

    P2P server

--- Blockchain/index.js
+++ Blockchain/index.js
@@ -17,7 +17,7 @@ class Blockchain {
 
     isValidChain(chain) {
         if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
-
+ 
         for (let i=1; i<chain.length; i++) {
             const block = chain[i];
             const lastBlock = chain[i-1];
